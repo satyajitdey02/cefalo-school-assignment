8c3459ab46893c8c2721a554330bc281e568ddc1
Satyajit Dey <satyajit02@gmail.com>
2016-11-15 01:38:58 +0600

    CSA: Client class refactored

--- src/main/java/com/cefalo/school/dp/week12/assignment/Client.java
+++ src/main/java/com/cefalo/school/dp/week12/assignment/Client.java
@@ -13,165 +13,77 @@ import java.util.Map;
 public class Client {
   private static final DatabaseManager databaseManager = new DatabaseManager();
   private static final DAO dao = new DAO();
+  private static final Command readCommand = new Read(dao);
+
+  private static Map<Integer, Entity> records = null;
 
   public static void main(String[] args) {
     Command createCommand = new Create(dao);
     databaseManager.setCommand(createCommand);
-
     databaseManager.saveRecord(new Entity("Neil deGrasse Tyson"));
     databaseManager.saveRecord(new Entity("Brian Greene"));
     databaseManager.saveRecord(new Entity("Michio Kaku"));
     databaseManager.saveRecord(new Entity("Satyajit Dey"));
-
-    Command readCommand = new Read(dao);
-    databaseManager.setCommand(readCommand);
-    Map<Integer, Entity> records = databaseManager.readRecords();
-
-    System.out.println("-------------------After saving Entities----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Save");
 
     Entity recordToUpdate = new Entity(records.get(1));
     recordToUpdate.setValue(recordToUpdate.getValue() + "-The Legend");
     Command updateCommand = new Update(dao);
     databaseManager.setCommand(updateCommand);
     databaseManager.updateRecord(recordToUpdate);
-
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After updating Entity----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Update");
 
     Entity lastRecord = records.get(records.size());
     Command deleteCommand = new Delete(dao);
     databaseManager.setCommand(deleteCommand);
     databaseManager.deleteRecord(lastRecord);
-
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After deleting Entity----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Delete");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.undoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Undo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Undo");
 
     databaseManager.redoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Redo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Redo");
 
     databaseManager.redoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Redo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Redo");
 
     databaseManager.redoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Redo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Redo");
 
     databaseManager.redoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Redo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Redo");
 
     databaseManager.redoOperation();
-    databaseManager.setCommand(readCommand);
-    records = databaseManager.readRecords();
-
-    System.out.println("-------------------After Redo----------------");
-    for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
-      System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
-    }
+    printRecords("Redo");
 
     databaseManager.undoOperation();
+    printRecords("Undo");
+  }
+
+  private static void printRecords(String operation) {
     databaseManager.setCommand(readCommand);
     records = databaseManager.readRecords();
 
-    System.out.println("-------------------After Undo----------------");
+    System.out.println(String.format("-------------------%s----------------", operation));
     for (Map.Entry<Integer, Entity> entry : records.entrySet()) {
       System.out.println(entry.getValue().getId() + " : " + entry.getValue().getValue());
     }
